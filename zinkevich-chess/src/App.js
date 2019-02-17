1840de0b77c33d2f7ff545d9fb475dd9ce0b16cb
VitaliZinkevich <vitalizinkevich@gmail.com>
2019-02-17 23:45:38 +0300

    trying react w chessboarjs like components

--- zinkevich-chess/src/App.js
+++ zinkevich-chess/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Test from './components/test'
 
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+    <div  id='board'></div>
      <Test></Test>
       </div>
     );
